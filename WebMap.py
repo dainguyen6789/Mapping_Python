382d024e2d085088f1d650508757d133ab8fcda4
DESKTOP-TR0VMRU\pc <ngdai6789@gmail.com>
2017-10-18 23:24:56 -0400

    Read world.json data

--- WebMap.py
+++ WebMap.py
@@ -24,8 +24,10 @@ def color_producer(elevation):
 map=folium.Map(location=[38.2,-99])
 fg=folium.FeatureGroup(name="My Map")
 for lt,ln,el in zip(lat,lon,elev):
+    fg.add_child(folium.CircleMarker (location=[lt,ln],popup=str(el)+'m',
+    fill_color=color_producer(el),color='grey',fill_opacity=0.7))
 
-    fg.add_child(folium.Marker (location=[lt,ln],popup=str(el)+'m',icon=folium.Icon(color=color_producer(el))))
-
+    #fg.add_child(folium.CircleMarker (location=[lt,ln],popup=str(el)+'m',icon=folium.Icon(color=color_producer(el))))
+fg.add_child(folium.GeoJson(data=(open('world.json','r',encoding='utf-8-sig').read())))
 map.add_child(fg)
 map.save("Map1.html")
