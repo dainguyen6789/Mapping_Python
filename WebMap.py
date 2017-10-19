4654c09c1b77a1e187f590cb623b00c14b2cc5e2
DESKTOP-TR0VMRU\pc <ngdai6789@gmail.com>
2017-10-18 23:59:46 -0400

    Add folium.LayerControl() (to be exactly, it is two feature groups)

--- WebMap.py
+++ WebMap.py
@@ -26,11 +26,14 @@ fg=folium.FeatureGroup(name="My Map")
 for lt,ln,el in zip(lat,lon,elev):
     fg.add_child(folium.CircleMarker (location=[lt,ln],radius=6,popup=str(el)+'m',
     fill_color=color_producer(el),color='grey',fill_opacity=0.7))
-
+fgp=folium.FeatureGroup(name='Population')
     #fg.add_child(folium.CircleMarker (location=[lt,ln],popup=str(el)+'m',icon=folium.Icon(color=color_producer(el))))
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read()
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read()
 , style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']
 <= 10000000 else 'orange' if 10000000 < x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
 map.save("Map1.html")
